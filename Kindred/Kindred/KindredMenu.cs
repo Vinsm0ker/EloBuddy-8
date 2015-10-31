a9ccbada4433d807baf5944be87e0a00547493aa
koka0012 <augusto.chmi@gmail.com>
2015-10-31 21:00:49 -0200

    Kindred Menu Reworked

---
+++ Kindred/Kindred/KindredMenu.cs
@@ -0,0 +1,190 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kindred
+{
+    class KindredMenu
+    {
+        public static Menu kindum, kincombo, kindraw, kinr, ks, kinlcs;
+        public static void loadMenu()
+        {
+            kindudepage();
+            kindrawpage();
+            kincombopage();       
+            //kinjcpage();
+            kinlcspage();
+            //kindrapage();
+            //kinkspage();
+            kinrpage();
+
+        }
+
+      
+
+        public static void kindudepage()
+        {
+            kindum = MainMenu.AddMenu("Kindred Dude", "main");
+            kindum.AddGroupLabel("About this script");
+            kindum.AddSeparator();
+            kindum.AddLabel(" Kindred Dude - " + Program.version);
+            kindum.AddLabel(" Made by -Koka and -iRaxe");
+            kindum.AddSeparator();
+            kindum.AddGroupLabel("Hotkeys");
+            kindum.AddLabel(" - Use SpaceBar for Combo");
+            kindum.AddLabel(" - Use the key V For LaneClear/JungleClear");
+
+        }
+
+        public static void kindrawpage()
+        {
+            kindraw = kindum.AddSubMenu("Draw  settings", "Draw");
+            kindraw.AddGroupLabel("Draw Settings");
+            kindraw.Add("nodraw", new CheckBox("No Display Drawing", false));
+            kindraw.Add("onlyReady", new CheckBox("Display only Ready", true));
+            kindraw.AddSeparator();
+            kindraw.Add("draw.Q", new CheckBox("Draw Q Range", true));
+            kindraw.Add("draw.W", new CheckBox("Draw W Range", true));
+            kindraw.Add("draw.E", new CheckBox("Draw E Range", true));
+            kindraw.Add("draw.R", new CheckBox("Draw R Range", true));
+        }
+
+        public static void kincombopage()
+        {
+            kincombo = kindum.AddSubMenu("Combo settings", "Combo");
+            kincombo.AddGroupLabel("Combo settings");
+            kincombo.AddSeparator();
+            kincombo.Add("combo.Q", new CheckBox("Use Q"));
+            kincombo.Add("combo.W", new CheckBox("Use W"));
+            kincombo.Add("combo.E", new CheckBox("Use E"));
+            kincombo.Add("combo.R", new CheckBox("Use R"));
+            kincombo.AddSeparator();
+            kincombo.AddLabel(" -Uncheck the boxes if you wish to dont use a specific spell while you are pressing the Combo Key");
+
+        }
+        public static void kinlcspage()
+        {
+            kinlcs = kindum.AddSubMenu("Lane Clear Settings", "laneclear");
+            kinlcs.AddGroupLabel("Lane clear settings");
+            kinlcs.AddSeparator();
+            kinlcs.Add("lc.Q", new CheckBox("Use Q"));
+            kinlcs.Add("lc.W", new CheckBox("Use W"));
+            kinlcs.Add("lc.Mana", new Slider("Min. Mana%",30));
+            kinlcs.Add("lc.MinionsQ", new Slider("Min. Mana%", 3,0,3));
+            kinlcs.Add("lc.MinionsW", new Slider("Min. Mana%", 3,0,10));
+            kinlcs.AddSeparator();
+            kinlcs.AddGroupLabel("Jungle Settings");
+            kinlcs.AddSeparator();
+            kinlcs.Add("jungle.Q", new CheckBox("Use Q jungle"));
+            kinlcs.Add("jungle.W", new CheckBox("Use E jungle"));
+            kinlcs.Add("jungle.E", new CheckBox("Use W jungle "));
+            kinlcs.AddSeparator();
+            kinlcs.AddLabel(" -Uncheck the boxes if you wish to dont use a specific spell while you are pressing the Combo Key");
+
+        }
+        public static void kinrpage()
+        {
+            kinr = kindum.AddSubMenu("R Menu", "rlogic");
+            kinr.AddGroupLabel("R Menu");
+            kinr.Add("rlogic.minhp", new Slider("Min. HP to use R", 30, 0, 100));
+            kinr.Add("rlogic.ehp", new Slider("Max enemy hp to use R", 10, 0, 100));
+            
+            foreach (var ally in ObjectManager.Get<Obj_AI_Base>().Where(o => o.IsAlly && !o.IsStructure() && !o.IsMinion && Program._Player.CanCast))
+            {
+                if (ally.BaseSkinName == "kindredwolf" || ally.BaseSkinName == "KindredWolf") return;
+                kinr.Add("r" + ally.BaseSkinName, new CheckBox("R on " + ally.BaseSkinName, true));
+            }
+        }
+        public void kinkspage()
+        {
+            ks = kindum.AddSubMenu("KillSteal settings", "ks");
+            ks.AddGroupLabel("KillSteal settings");
+            ks.AddSeparator();
+            ks.Add("ksq", new CheckBox("Ks using Q"));
+            ks.Add("ksi", new CheckBox("Ks using Ignite[WIP]"));
+        }
+
+
+
+        public static int rLogicMinHp()
+        {
+            return kinr["rlogic.minhp"].Cast<Slider>().CurrentValue;
+        }
+        public static int rLogicEnemyMinHp()
+        {
+            return kinr["rlogic.ehp"].Cast<Slider>().CurrentValue;
+        }
+        public static bool ksQ()
+        {
+            return ks["ksq"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool useWjungle()
+        {
+            return kinlcs["jungle.W"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool useEjungle()
+        {
+            return kinlcs["jungle.E"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool useQjungle()
+        {
+            return kinlcs["jungle.Q"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool useQ()
+        {
+            return kinlcs["combo.Q"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool useW()
+        {
+            return kinlcs["combo.W"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool useE()
+        {
+            return kinlcs["combo.E"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool nodraw()
+        {
+            return kindraw["nodraw"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool onlyReady()
+        {
+            return kindraw["onlyready"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool drawingsQ()
+        {
+            return kindraw["draw.Q"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool drawingsW()
+        {
+            return kindraw["draw.W"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool drawingsE()
+        {
+            return kindraw["draw.E"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool drawingsR()
+        {
+            return kindraw["draw.R"].Cast<CheckBox>().CurrentValue;
+        }
+
+        public static bool useQlc()
+        {
+            return kinlcs["lc.Q"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool useWlc()
+        {
+            return kinlcs["lc.W"].Cast<CheckBox>().CurrentValue;
+        }
+        public static bool useElc()
+        {
+            return kinlcs["lc.E"].Cast<CheckBox>().CurrentValue;
+        }
+    }
+}
+
--- Kindred/Kindred/Program.cs
+++ Kindred/Kindred/Program.cs
@@ -5,6 +5,10 @@ using EloBuddy.SDK.Events;
 using EloBuddy.SDK.Menu;
 using EloBuddy.SDK.Menu.Values;
 using EloBuddy.SDK.Rendering;
+using SharpDX;
+
+using Color = System.Drawing.Color;
+
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -36,11 +40,7 @@ namespace Kindred
         static void Main(string[] args)
         {
             Loading.OnLoadingComplete += OnLoadingComplete;
-            Game.OnUpdate += OnGameUpdate;
-            Drawing.OnDraw += GameOnDraw;
             
-            Gapcloser.OnGapcloser += AntiGapCloser;
-            Obj_AI_Base.OnProcessSpellCast += OnProcessSpellCast;
         }
 
 
@@ -49,9 +49,9 @@ namespace Kindred
         {
             if (Player.Instance.ChampionName != "Kindred") return;
 
-            Chat.Print("Kindred Dude Loaded!", Color.CornflowerBlue);
+            Chat.Print("Kindred Dude Loaded!",Color.CornflowerBlue);
 
-            
+            Game.OnTick += GameOnTick;
 
             #region Skill
 
@@ -62,77 +62,14 @@ namespace Kindred
 
             #endregion
 
+            KindredMenu.loadMenu();
 
+            Game.OnUpdate += OnGameUpdate;
+            Drawing.OnDraw += GameOnDraw;
 
+            Gapcloser.OnGapcloser += AntiGapCloser;
+            //Obj_AI_Base.OnProcessSpellCast += OnProcessSpellCast;
 
-            #region 
-
-            Kindred = MainMenu.AddMenu("Kindred Dude", "kindude");
-            Kindred.AddGroupLabel("Kindred Dude - " + version);
-            Kindred.AddLabel("Made by -Koka");
-
-            Draw = Kindred.AddSubMenu("Draw Menu", "kindra");
-            Draw.AddGroupLabel("Draw Settings");
-            Draw.Add("nodraw", new CheckBox("No Display Drawing", false));
-            Draw.Add("onlyR", new CheckBox("Display only Ready", true));
-            Draw.AddSeparator();
-            Draw.Add("drawq", new CheckBox("Draw Q Range", true));
-            Draw.Add("drawW", new CheckBox("Draw W Range", true));
-            Draw.Add("drawE", new CheckBox("Draw E Range", true));
-            Draw.Add("drawr", new CheckBox("Draw R Range", true));
-
-            Combo = Kindred.AddSubMenu("Combo Menu", "kincombo");
-            Combo.AddGroupLabel("Combo");
-            Combo.Add("useq", new CheckBox("Use Q in Combo", true));
-            Combo.Add("usew", new CheckBox("Use W in Combo", true));
-            Combo.Add("usee", new CheckBox("Use E in Combo", true));
-
-            Ks = Kindred.AddSubMenu("KillSteal Menu", "kinks");
-            Ks.AddGroupLabel("Kill Steal");
-            Ks.Add("ksq", new CheckBox("Ks with Q", true));
-            Ks.Add("ksi", new CheckBox("Ks with Ignite [WIP]", true));
-
-            LaneClear = Kindred.AddSubMenu("LaneClear Settings", "kinlcs");
-            LaneClear.AddGroupLabel("LaneClear Settings");
-            LaneClear.Add("qlane", new CheckBox("Use Q in LaneClear", true));
-            LaneClear.Add("wlane", new CheckBox("Use W in LaneClear", true));
-            LaneClear.Add("qcount", new Slider("Min. Minions to Q", 3, 1, 3));
-            LaneClear.Add("wcount", new Slider("Min. Minions to W", 5, 1, 20));
-            LaneClear.Add("mana", new Slider("Min. Mana %", 30, 0, 100));
-            LaneClear.Add("min", new Slider("Min. Minions to Q", 2, 0, 10));
-            LaneClear.Add("wmin", new Slider("Min. Minions to W", 2, 0, 10));
-
-            
-
-            Jungle = Kindred.AddSubMenu("Jungle Settings", "kinjc");
-            Jungle.AddGroupLabel("Jungle Settings");
-            Jungle.Add("qjungle", new CheckBox("Use Q in Jungle", true));
-            Jungle.Add("wjungle", new CheckBox("Use W in Jungle", true));
-            Jungle.Add("ejungle", new CheckBox("Use E in Jungle", true));
-            Jungle.Add("manaj", new Slider("Min. Mana %", 30, 0, 100));
-
-            
-
-
-
-            
-        
-
-            Misc = Kindred.AddSubMenu("Misc Menu", "kinmisc");
-            Misc.AddGroupLabel("AntiGap Closer -BETA");
-            Misc.Add("antigp", new CheckBox("AntiGap Closer With Q", true));
-
-
-            Game.OnTick += GameOnTick;
-
-            CreateRMenu();
-
-
-
-
-
-
-            #endregion
 
 
         }
@@ -144,34 +81,8 @@ namespace Kindred
 
             KillSteal();
             RLogic();
+            //Activator();
 
-
-        }
-        public static void CreateRMenu()
-        {
-            Rmenu = Kindred.AddSubMenu("R Menu", "kinr");
-            Rmenu.AddGroupLabel("R Menu");
-            Rmenu.Add("minhp", new Slider("Min. HP to use R", 30, 0, 100));
-            Rmenu.Add("ehp", new Slider("Max enemy hp to use R", 10, 0, 100));
-
-            foreach (var ally in ObjectManager.Get<Obj_AI_Base>().Where(o => o.IsAlly && !o.IsStructure() && !o.IsMinion && _Player.CanCast))
-            {
-                if (ally.BaseSkinName == "kindredwolf") return;
-                Rmenu.Add("r" + ally.BaseSkinName, new CheckBox("R on " + ally.BaseSkinName, true));
-            }
-
-            Rmenu.AddSeparator();
-            Rmenu.AddGroupLabel("Protector Skill Whitelist");
-            Rmenu.Add("dprotector", new CheckBox("Disable Protector", false));
-            Rmenu.Add("hpprotector", new Slider("Min. Hp Protector", 10, 0, 100));
-
-            foreach (var enemy in ObjectManager.Get<Obj_AI_Base>().Where(o => o.IsEnemy))
-            {
-                foreach (var skillshot in SpellDB.SpellDatabase.Spells.Where(x => x.charName == enemy.BaseSkinName))
-                {
-                    Rmenu.Add("champ." + skillshot.spellName, new CheckBox(skillshot.spellName + " | " + skillshot.charName, true));
-                }
-            }
 
         }
         private static void AntiGapCloser(AIHeroClient sender, Gapcloser.GapcloserEventArgs gapcloser)
@@ -191,26 +102,26 @@ namespace Kindred
 
         public static void GameOnDraw(EventArgs args)
         {
-            if (Draw["nodraw"].Cast<CheckBox>().CurrentValue) return;
+            if (KindredMenu.nodraw()) return;
 
 
 
 
-            if (!Draw["onlyR"].Cast<CheckBox>().CurrentValue)
+            if (!KindredMenu.onlyReady())
             {
-                if (Draw["drawq"].Cast<CheckBox>().CurrentValue)
+                if (KindredMenu.drawingsQ())
                 {
                     new Circle() { Color = Color.AliceBlue, Radius = 340, BorderWidth = 2f }.Draw(_Player.Position);
                 }
-                if (Draw["draww"].Cast<CheckBox>().CurrentValue)
+                if (KindredMenu.drawingsW())
                 {
                     new Circle() { Color = Color.OrangeRed, Radius = 800, BorderWidth = 2f }.Draw(_Player.Position);
                 }
-                if (Draw["drawe"].Cast<CheckBox>().CurrentValue)
+                if (KindredMenu.drawingsE())
                 {
                     new Circle() { Color = Color.Cyan, Radius = 500, BorderWidth = 2f }.Draw(_Player.Position);
                 }
-                if (Draw["drawr"].Cast<CheckBox>().CurrentValue)
+                if (KindredMenu.drawingsR())
                 {
                     new Circle() { Color = Color.SkyBlue, Radius = 500, BorderWidth = 2f }.Draw(_Player.Position);
                 }
@@ -218,22 +129,22 @@ namespace Kindred
             }
             else
             {
-                if (!Q.IsOnCooldown && Draw["drawq"].Cast<CheckBox>().CurrentValue)
+                if (!Q.IsOnCooldown && KindredMenu.drawingsQ())
                 {
 
                     new Circle() { Color = Color.AliceBlue, Radius = 340, BorderWidth = 2f }.Draw(_Player.Position);
                 }
-                if (!W.IsOnCooldown && Draw["draww"].Cast<CheckBox>().CurrentValue)
+                if (!W.IsOnCooldown && KindredMenu.drawingsW())
                 {
 
                     new Circle() { Color = Color.OrangeRed, Radius = 800, BorderWidth = 2f }.Draw(_Player.Position);
                 }
-                if (!E.IsOnCooldown && Draw["drawe"].Cast<CheckBox>().CurrentValue)
+                if (!E.IsOnCooldown && KindredMenu.drawingsE())
                 {
 
                     new Circle() { Color = Color.Cyan, Radius = 500, BorderWidth = 2f }.Draw(_Player.Position);
                 }
-                if (!R.IsOnCooldown && Draw["drawr"].Cast<CheckBox>().CurrentValue)
+                if (!R.IsOnCooldown && KindredMenu.drawingsR())
                 {
 
                     new Circle() { Color = Color.SkyBlue, Radius = 500, BorderWidth = 2f }.Draw(_Player.Position);
@@ -257,17 +168,17 @@ namespace Kindred
             var alvo = TargetSelector.GetTarget(1000, DamageType.Physical);
             if (alvo == null || !alvo.IsValid || alvo.IsDead || alvo.IsZombie || _Player.IsDead) return;
 
-            if (Q.IsReady() && Combo["useq"].Cast<CheckBox>().CurrentValue && alvo.Distance(_Player) <= 500)
+            if (Q.IsReady() && KindredMenu.useQ() && alvo.Distance(_Player) <= 500)
             {
                 Q.Cast(Game.ActiveCursorPos);
             }
 
-            if (W.State == SpellState.Ready && Combo["usew"].Cast<CheckBox>().CurrentValue && alvo.Distance(_Player) <= 700)
+            if (W.State == SpellState.Ready && KindredMenu.useE() && alvo.Distance(_Player) <= 700)
             {
                 W.Cast();
             }
 
-            if (E.IsReady() && Combo["usee"].Cast<CheckBox>().CurrentValue && alvo.Distance(_Player) <= 500)
+            if (E.IsReady() && KindredMenu.useW() && alvo.Distance(_Player) <= 500)
             {
                 E.Cast(alvo);
             }
@@ -276,12 +187,13 @@ namespace Kindred
         public static void OnLaneClear()
         {
             var Minions = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy,_Player.Position,540).Count();
-            var q = LaneClear["qlane"].Cast<CheckBox>().CurrentValue;
-            var w = LaneClear["wlane"].Cast<CheckBox>().CurrentValue;
-            var mp = LaneClear["mana"].Cast<Slider>().CurrentValue;
-            var min = LaneClear["min"].Cast<Slider>().CurrentValue;
-            var wmin = LaneClear["wmin"].Cast<Slider>().CurrentValue;
+            var q = KindredMenu.useQlc();
+            var w = KindredMenu.useWlc();
+            var mp = KindredMenu.kinlcs["lane.Mana"].Cast<Slider>().CurrentValue;
+            var min = KindredMenu.kinlcs["lane.MinionsQ"].Cast<Slider>().CurrentValue;
+            var wmin = KindredMenu.kinlcs["lane.MinionsW"].Cast<Slider>().CurrentValue;
 
+            if (Minions == 0) return;
 
             if (_Player.ManaPercent < mp) return;
 
@@ -299,10 +211,9 @@ namespace Kindred
 
         public static void OnJungle()
         {
-            var q = Jungle["qjungle"].Cast<CheckBox>().CurrentValue;
-            var w = Jungle["wjungle"].Cast<CheckBox>().CurrentValue;
-            var e = Jungle["ejungle"].Cast<CheckBox>().CurrentValue;
-            var mana = Jungle["manaj"].Cast<Slider>().CurrentValue;
+            var q = KindredMenu.useQjungle();
+            var w = KindredMenu.useWjungle();
+            var e = KindredMenu.useEjungle();
             var monster = EntityManager.MinionsAndMonsters.GetJungleMonsters(_Player.Position, W.Range).ToArray();
 
 
@@ -312,8 +223,8 @@ namespace Kindred
                 return;
             }
 
-            if (_Player.ManaPercent >= mana)
-            {
+            //if (_Player.ManaPercent >= mana)
+            //{
                 if (Q.IsReady() && q )
                 {
                     Q.Cast(Game.CursorPos);
@@ -326,7 +237,7 @@ namespace Kindred
                 {
                     E.Cast(monster[0]);
                 }
-            }
+            //}
 
 
 
@@ -340,8 +251,8 @@ namespace Kindred
             var ap = _Player.FlatMagicDamageMod + _Player.BaseAbilityDamage;
             var ad = _Player.FlatMagicDamageMod + _Player.BaseAttackDamage;
 
-            var useq = Ks["ksq"].Cast<CheckBox>().CurrentValue;
-            var usei = Ks["ksi"].Cast<CheckBox>().CurrentValue;
+            var useq = KindredMenu.ksQ();
+            //var usei = Ks["ksi"].Cast<CheckBox>().CurrentValue;
 
             if (Q.IsReady() && useq && !Target.IsZombie && Target.Health < Damage.CalculateDamageOnUnit(_Player,Target,DamageType.Physical, 60 + ((Q.Level - 1) * 30) + ad/5))
             {
@@ -352,23 +263,22 @@ namespace Kindred
 
         public static void RLogic()
         {
-            var mHP = Rmenu["minhp"].Cast<Slider>().CurrentValue;
-            var eHP = Rmenu["ehp"].Cast<Slider>().CurrentValue;
-
+            var mHP = KindredMenu.rLogicMinHp();
+            var eHP = KindredMenu.rLogicEnemyMinHp();
             foreach (var ally in EntityManager.Heroes.Allies.Where(o => o.HealthPercent < mHP && !o.IsRecalling() && !o.IsDead && !o.IsZombie && _Player.Distance(o.Position) < R.Range && !o.IsInShopRange()))
             {
                 var enemy = EntityManager.Heroes.Enemies.Where(o => ally.Distance(o.Position) <= 800 && o.HealthPercent <= eHP).ToArray();
                 if (enemy.Length != 0) return;
                 
 
-                if (Rmenu["r" + ally.BaseSkinName].Cast<CheckBox>().CurrentValue && ally.CountEnemiesInRange(800) >= 1)
+                if (KindredMenu.kinr["r" + ally.BaseSkinName].Cast<CheckBox>().CurrentValue && ally.CountEnemiesInRange(800) >= 1)
                 {
                     R.Cast(ally);
                 }
 
             }
         }
-        public static void OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs spell)
+        /*public static void OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs spell)
         {
             var hp = Rmenu["hpprotector"].Cast<Slider>().CurrentValue;
             if (!R.IsReady() && _Player.IsDead && _Player.IsZombie && sender.IsAlly && !sender.IsMe && !Rmenu["dprotector"].Cast<CheckBox>().CurrentValue) return;
@@ -390,12 +300,18 @@ namespace Kindred
                         R.Cast(_Player);
                     }
                 }
-            
-
-            
+            }
+        }
+        public static void Activator()
+        {
+            if (Item.HasItem(ItemId.Blade_of_the_Ruined_King))
+            {
+                var target = TargetSelector.GetTarget(500, DamageType.Physical);
+                Item.UseItem(ItemId.Blade_of_the_Ruined_King, target);
             }
 
-        }
+
+        }*/
         
 
 
