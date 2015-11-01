9a4721b77b6c29da3029dfe4d6d57e3eb0137c74
iRaxe <djvinx@live.com>
2015-11-01 17:43:15 +0100

    Update KindredItems.cs
    Deleted use of youmu/bork from here

--- Kindred/Kindred/KindredItems.cs
+++ Kindred/Kindred/KindredItems.cs
@@ -12,15 +12,12 @@ namespace Kindred
     class KindredItems
     {
         public static Spell.Targeted ignite;
-        public static Item youmus,botrk;
         public static void loadSpells()
         {
             if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner1).Name.Contains("dot"))
                 ignite = new Spell.Targeted(SpellSlot.Summoner1, 580);
             else if (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Summoner2).Name.Contains("dot"))
                 ignite = new Spell.Targeted(SpellSlot.Summoner2, 580);
-            youmus = new Item(3142);
-            botrk = new Item(3153, 500);
         }
     }
 }
